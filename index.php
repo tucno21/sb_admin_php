decd43f405f0613788f155279b74dee033c51a4b
tucno21 <carlitostucno@gmail.com>
2021-10-07 18:02:13 -0500

    sb-admin blue

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+//cabeza de la pplantilla
+include 'sb-admin/sbAdmin.php';
+include 'sb-admin/head.php';
+?>
+
+<!-- <div class="container-fluid px-2">
+    <h2 class="mt-1">Dashboard 2</h2>
+
+</div> -->
+
+<!-- Begin Page Content -->
+<!-- <div class="container-fluid">
+    <h1 class="mb-2 text-gray-800 h3">Roles</h1>
+
+</div> -->
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+
+</div>
+<!-- /.container-fluid -->
+
+
+
+<?php
+//cabeza de la pplantilla
+include 'sb-admin/footer.php';
+?>
